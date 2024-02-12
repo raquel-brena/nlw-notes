dd02f4617faaa4772872c5a06f7f13b17523a287
Raquel <raquel.lima.124@ufrn.edu.br>
2024-02-12 17:32:48 -0300

    feat: Add search functionality and note deletion

--- nlw-notes/src/app.tsx
+++ nlw-notes/src/app.tsx
@@ -1,15 +1,62 @@
+import { useState } from "react";
 import logo from "./assets/logo-nlw.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
+import { toast } from "sonner";
 
-const note = {
-  date: new Date(),
-  content: "Hello World"
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
 }
-
 export function App() {
-  return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notes = localStorage.getItem("notes");
+    if (notes) {
+      return JSON.parse(notes);
+    }
+    return [];
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function onNoteDelete(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray);
+
+    toast.success("Nota deletada com sucesso!")
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+    setSearch(query);
+  }
+
+ // const filteredNotes = notes.filter((note) => {
+   // return note.content.toLowerCase().includes(search.toLowerCase());
+  //});
+const filteredNotes = search !== '' ? 
+notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) 
+: notes
+ 
+return (
+    <div className="mx-auto max-w-6xl my-12 space-y-5 px-5 md:px-0">
       <img src={logo} alt="Logo" />
 
       <form className="w-full">
@@ -17,18 +64,16 @@ export function App() {
           type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-      
-        <NewNoteCard/>
-        <NoteCard note={note} />
-        <NoteCard note={note} />
-        <NoteCard note={note} />
-        <NoteCard note={note} />
-
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {filteredNotes.map((note) => (
+          <NoteCard key={note.id} note={note}  onNoteDelete={onNoteDelete}/>
+        ))}
       </div>
     </div>
   );
