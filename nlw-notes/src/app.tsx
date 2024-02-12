e48c497a6245e7e7e896ec3c074b6967a57d1636
Raquel <raquel.lima.124@ufrn.edu.br>
2024-02-12 14:14:08 -0300

    feat: Add logo, new note card, and note card components

--- nlw-notes/src/app.tsx
+++ nlw-notes/src/app.tsx
@@ -1,11 +1,37 @@
+import logo from "./assets/logo-nlw.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
 
+const note = {
+  date: new Date(),
+  content: "Hello World"
+}
 
 export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="Logo" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+      
+        <NewNoteCard/>
+        <NoteCard note={note} />
+        <NoteCard note={note} />
+        <NoteCard note={note} />
+        <NoteCard note={note} />
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
